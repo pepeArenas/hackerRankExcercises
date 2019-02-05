f8f61e1ab55d24b8b2ceed84bf308739fa0707ad
jesus <jesusarenasm>
2019-02-05 13:57:19 -0600

    This is an example that shows the inclusive and exclusive behavior of the substring method

---
+++ src/main/java/Substring.java
@@ -0,0 +1,9 @@
+public class Substring {
+    public static void main(String[] args) {
+        String word = "Helloworld";
+        int start = 3;
+        int end = 7;
+        System.out.println("The only thing to remember here is the START element is inclusive and the END exclusive");
+        System.out.println(word.substring(start, end));
+    }
+}
